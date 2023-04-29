21cdfe3dbfa838cf94b073e8c6be0716255ea021
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-04-29 15:43:38 +0100

    created models

--- partypal/base/models.py
+++ partypal/base/models.py
@@ -28,4 +28,39 @@ class Event(models.Model):
         return self.event_name
     
 
+class Ticket(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='tickets')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    purchased_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.event} - {self.quantity} - {self.total}'
+    
+    # def save(self, *args, **kwargs):
+
+    #     self.total = self.quantity * self.price
+    #     super().save(*args, **kwargs)
+
+
+class Attendee(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='attendees')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attendees')
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name='attendees')
+    purchase_date = models.BooleanField(default=False)
+    # checked_in_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.event} - {self.checked_in}'
+
+
+
+class Organizer(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='organizers')
+    email_address = models.ForeignKey(User, on_delete=models.CASCADE, related_name='organizers')
+    
+
+class Venue
 
--- partypal/base/serializers.py
+++ partypal/base/serializers.py
@@ -6,3 +6,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['pk', 'name', 'email', 'password']
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ['pk', 'name', 'description', 'date', 'location', 'host', 'guests']
