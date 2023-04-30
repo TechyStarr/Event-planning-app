5df7cd541ac6cbec63e597caf28c4b7778c9e756
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-04-30 15:31:26 +0100

    created home route

--- partypal/base/views.py
+++ partypal/base/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, redirect
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Event
+from .serializers import EventSerializer, UserSerializer
 
 # Create your views here.
 
+
+@api_view(['GET'])
 def home(request):
-    return redirect
+    routes = [
+        'GET/api',
+        'GET/api/rooms',
+        'GET/api/rooms/:id',
+    ]
+    return Response(routes)
+
+class EventViewset(viewsets.ModelViewSet):
+    query_set = Event.objects.all()
+    serializer_class  = EventSerializer
