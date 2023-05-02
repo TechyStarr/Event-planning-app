91e070c0f475e694525123060a9c0b6fe2a3c885
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-05-02 10:33:06 +0100

    created custom user model

--- partypal/api/models.py
+++ partypal/api/models.py
@@ -1,8 +1,27 @@
+from typing import Any
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager, AbstractUser
 
 
 # Create your models here.
+
+# class CustomUserManager(BaseUserManager):
+#     def create_user(self, username, password, **extra_fields):
+#         if not username:
+#             raise ValueError('The username must be set')
+#         user = self.model(username=username, **extra_fields)
+#         user.set_password(password)
+#         user.save()
+#         return user
+    
+#     def create_superuser(self, username, password, **extra_fields):
+#         extra_fields.setdefault('is_organizer', True)
+#         extra_fields.setdefault('is_guest', True)
+#         extra_fields.setdefault('super_user', True)
+#         return self.create_user(username, password, **extra_fields)
+    
+
+
 
 
 class User(AbstractUser):
--- partypal/api/views.py
+++ partypal/api/views.py
@@ -2,30 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User, Event, Guest, Host, Venue
 from .serializers import UserSerializer, EventSerializer, GuestSerializer, HostSerializer, VenueSerializer
 
-@api_view(['GET'])
-def userList(request):
-    users = User.objects.all()
-    serializers = UserSerializer(users, many=True)
-    return Response(serializers.data)
-
-
-@api_view(['GET'])
-def createUser(request):
-    users = User.objects.all()
-    serializers = UserSerializer(data=request.data)
-    if serializers.is_valid():
-        serializers.save()
-    return Response(serializers.data)
 
 
 @api_view(['GET'])
 def apiOverview(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
     api_urls = {
         'List':'/event-list/',  
         'Detail View':'/event-detail/<str:pk>/',
@@ -35,6 +23,52 @@ def apiOverview(request):
     }
     return Response(api_urls) # safe=False allows for lists to be returned
 
+
+
+
+# ------------- USER -----------------
+@api_view(['GET'])
+def userList(request):
+    users = User.objects.all()
+    serializers = UserSerializer(users, many=True)
+    return Response(serializers.data)
+
+
+@api_view(['GET'])
+def userDetail(request, pk):
+    users = User.objects.get(id=pk)
+    serializer  = UserSerializer(users, many=False)
+
+
+@api_view(['POST'])
+def createUser(request):
+    users = User.objects.all()
+    # serializers = UserSerializer(data=request.data)
+    # if serializers.is_valid():
+    #     serializers.save()
+
+
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        # return Response(serializer.errors)
+    # return Response(serializers.data)
+
+@api_view(['PUT'])
+def updateUser(request, pk):
+    users = User.objects.get(id=pk)
+    serializers = UserSerializer(instance=users, data=request.data)
+    if serializers.is_valid():
+        serializers.save()
+    return Response(serializers.data)
+
+
+
+
+
+# ------------- EVENT -----------------
 @api_view(['GET'])
 def eventList(request):
     events = Event.objects.all()
