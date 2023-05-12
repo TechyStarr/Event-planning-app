b1c8d825e05be9a9c07910de5b63ed629a955ba8
Calista Ifenkwe <calistaifenkwe@gmail.com>
2023-05-12 22:59:53 +0100

    profile created

--- partypal/api/views.py
+++ partypal/api/views.py
@@ -149,7 +149,18 @@ class GuestInvite(APIView):
             return Response({"error": "Event not found"})
         
 
-        
+class ViewHost(APIView):
+    @authentication_classes([JWTAuthentication])
+    @permission_classes([IsAuthenticated])
+
+    def get(self, request, event_id):
+        event = Event.objects.get(id=event_id)
+        if event:
+            host = event.host
+            serializer = HostSerializer(host)
+            return Response(serializer.data)
+        else:
+            return Response({"error": "Event not found"})
 
 
 
@@ -198,9 +209,7 @@ def unregisterForEvent(request, pk):
     pass
 
 
-@api_view(['GET'])
-def inviteUser(request, pk):
-    pass
+
 
 @api_view(['GET'])
 def uninviteUser(request, pk):
